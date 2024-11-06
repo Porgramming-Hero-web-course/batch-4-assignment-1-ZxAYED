9620786ba4e73f5e5958fed96dc1b97b12089f6f
Zayed Iqbal <zzayedghost@gmail.com>
2024-11-06 21:03:27 +0600

    pblm 3 solved

--- TypeScript-problem.ts
+++ TypeScript-problem.ts
@@ -12,8 +12,6 @@ let sum =0
  }
 console.log(sumArray());
 
-//   description : to summerize all numbers i firstly dceclair the sum initial value . then in a sumArray function using forEach method   . i can use map method too. but i prefer forEach as i have to write maps while developing . after the value i have to return the value as mentioned in the question.
-
 
 //  problem 2.
  const array2:ArrayOfNumber = [ 123,1231,1,1,22,22,22,3,4,4,4,5,5,6,6,6,6,1]
@@ -31,6 +29,19 @@ function removeDuplicates():ArrayOfNumber {
 
 console.log(removeDuplicates());
 
-// description:  wasted  alot time investing finding the suitable atrribute  for the solution. i figured out that i can work with filter option  then i used if method to compare with my newly created  empty array . then i can use index of or includes the old lvl 1 method that i forgot name but i knew something like this exist . afterwards i compare the value with newly created array to the old one and it creats the new array i need
+
 
 //  problem -3 
+ const countWordOccurrences=(sentence:string,word:string):number=>{
+const splitSentence = sentence.toLowerCase().split(' ')
+
+const filteredArray = splitSentence.filter((i)=>i===word.toLowerCase())
+
+const result = filteredArray.length
+return result
+
+
+
+
+ }
+ console.log(countWordOccurrences(' Keno ei nisshongota,  keNo ei mounota ', 'KENO'));
